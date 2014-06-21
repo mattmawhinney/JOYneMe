ec74f2c9fcb9518565cc15ff175cef3114c11bc6
Matt Mawhinney <matt.mawhinney@gmail.com>
2014-06-21 11:56:35 -0400

    changed signout route from GET to DELETE

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resource :sessions
   resources :users
 
-  get '/signout', to: 'sessions#destroy'
+  delete '/signout', to: 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
