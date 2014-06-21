50c67aea731fafc296dc0c59a248f670dcf8f4d2
Matt Mawhinney <matt.mawhinney@gmail.com>
2014-06-21 13:41:59 -0400

    added minimum password length validation to user.rb and password_confirmation field to user views _form; password confirmation validation from bcrypt not working

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,17 @@ class User < ActiveRecord::Base
 
 	EmailRegex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-	validates_presence_of :username, :email, :password
+	#password confirmation through input fields currently not working
+	has_secure_password 
+	validates_length_of   :password, :minimum => 8
+	validates_presence_of :username, :email
 	validates_length_of   :username, :maximum => 30
 	validates_format_of   :email, :with => EmailRegex
-	#need to double check and make sure I am ignoring case in validating uniqueness
+	# need to double check and make sure I am ignoring case in validating uniqueness
 	validates_uniqueness_of :username, :email, :case_sensitive => false
 
 
 
 	has_many :events
-	has_secure_password 
+	
 end
