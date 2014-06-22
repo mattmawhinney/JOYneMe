3567e6018b1a487cc0bb3a10221933781eef8192
Matt Mawhinney <matt.mawhinney@gmail.com>
2014-06-22 15:58:10 -0400

    changed definiton in events controller to allow for destroying events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -75,7 +75,7 @@ class EventsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_event
-      @event = current_user.notes.find(params[:id])
+      @event = current_user.events.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
