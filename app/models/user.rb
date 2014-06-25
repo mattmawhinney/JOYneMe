9be5b35468e71146e5ab9b6b1a6f51c10799b65b
Matt Mawhinney <matt.mawhinney@gmail.com>
2014-06-25 11:56:56 -0400

    modified user model to include paperclip has_attached_file and validates; created db migration adding avatar column to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,10 +11,11 @@ class User < ActiveRecord::Base
 	# need to double check and make sure I am ignoring case in validating uniqueness
 	validates_uniqueness_of :username, :email, :case_sensitive => false
 
-
-
 	has_many :events
 	has_many :attendees
+
+	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
 	
 end
