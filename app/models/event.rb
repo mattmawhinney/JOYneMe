9805979f0cc6d5f940ff0460faad162d82de68d3
Matt Mawhinney <matt.mawhinney@gmail.com>
2014-06-28 20:27:51 -0400

    added validations to event model and altered event form

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,9 @@ class Event < ActiveRecord::Base
   has_many :attendees
   has_many :users, through: :attendees
 
+  validates_presence_of :description, :neighborhood, :zip_code, :street_address
+  validates_length_of   :description, :maximum => 100
+
 
     # class method for sorting all events by time 
   def self.order_by_time
